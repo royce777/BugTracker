5a617167a8bb34d907d44cef6538f96ffa64537d
royce777 <marioboom10@gmail.com>
2022-07-27 00:45:01 -0300

    TicketChangeController + Ticket history partial view

---
+++ BugTracker/Controllers/TicketChangeController.cs
@@ -0,0 +1,20 @@
+﻿using BugTracker.UnitOfWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracker.Controllers
+{
+    public class TicketChangeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public TicketChangeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index(int ticketId)
+        {
+            var changes = _unitOfWork.TicketChanges.GetForTicket(ticketId);
+            return PartialView("/Views/Ticket/_ChangesPartialView.cshtml", changes);
+        }
+
+    }
+}
